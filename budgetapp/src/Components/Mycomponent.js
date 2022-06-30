3c745a9c9c9410fbd7fbae3697fc83a8d7f2803e
WijdaneCHAR <wijdanecharkaoui@gmail.com>
2022-06-30 13:16:25 +0100

    final

--- budgetapp/src/Components/Mycomponent.js
+++ budgetapp/src/Components/Mycomponent.js
@@ -1,5 +1,6 @@
 import React , {useState , useRef , useEffect }from 'react'
-import { Button , Form ,MoneyDiv,Input, Checkbox, Reset, Ul, Li, Remove}from '../Styling';
+import { Button , Form ,MoneyDiv,Input, Checkbox, Reset, Ul, Li, Remove, Description,Amounts, DivScroll}from '../Styling'
+import swal from 'sweetalert'
 export default function () {
   function useLocalStorage(key, initialValue) {
     const [storedValue, setStoredValue] = useState(() => {
@@ -32,7 +33,6 @@ export default function () {
     const [couple,setCouple] = useLocalStorage("couple",{})
     const [income,setIncome] = useLocalStorage("income",0);
     const [expense,setExpense] = useLocalStorage("expense",0);
-    const [err,setErr] = useState(false);
 
     const prevIncome = useRef();
     const prevExpense = useRef();
@@ -44,12 +44,36 @@ export default function () {
   const add = (e) => {
     e.preventDefault();
     const newCouple = {};
-    newCouple[e.target.elements.amount.value] = e.target.elements.description.value; 
-    setCouple(Object.assign({}, couple, newCouple));
-    if(e.target.elements.incomeType.checked)
+    console.log(isNaN(Number(e.target.elements.amount.value)))
+    if(isNaN(Number(e.target.elements.amount.value))){
+      swal({
+        text:"The amount should be a number ...",
+        buttons:false,
+        icon:'error',
+        timer:1000
+      })
+    }
+    else{
+       if(e.target.elements.incomeType.checked){
+      newCouple[e.target.elements.amount.value] = e.target.elements.description.value; 
+      setCouple(Object.assign({}, couple, newCouple));
       setIncome(prevIncome.current + Number(e.target.elements.amount.value));
-    else if(e.target.elements.expenseType.checked)  setExpense(prevExpense.current + Number(e.target.elements.amount.value)); 
-    else setErr(true);
+    }
+    else if(e.target.elements.expenseType.checked){
+      newCouple[e.target.elements.amount.value] = e.target.elements.description.value; 
+      setCouple(Object.assign({}, couple, newCouple));
+      setExpense(prevExpense.current + Number(e.target.elements.amount.value)); 
+    } 
+    else {
+      swal({
+        text:"Check an amount type to continue ...",
+        buttons:false,
+        icon:'error',
+        timer:2000
+      })
+    };
+    }
+   
     e.target.elements.amount.value ="";
     e.target.elements.description.value ="";
     e.target.elements.incomeType.checked = false;
@@ -59,62 +83,107 @@ export default function () {
   const reset = () =>{
     setIncome(0);
     setExpense(0);
-    setErr(false);
   }
 
-  const clear = () =>{
-    setErr(false);
-  }
 
   return (
-    <div>
+    <div style={{backgroundColor:"#EEEEEE",paddingBottom: "100px"}}>
         <MoneyDiv>
-            <div><h5>Money Earned : </h5>{income} $</div>
-            <div><h5>Money Available : </h5>{income-expense} $</div>
-            <div><h5>Money Spent : </h5>{expense} $</div> 
+            <div><p>Money Available : </p><h1 style={{fontSize:"45px"}}>$ {income-expense}</h1></div>
+            <div style={{display:"flex",flexDirection:"row",textAlign:"center"}}>
+              <div style={{width:"50%",color:"#395A86",}}>
+                <DivScroll style={{backgroundColor:"#ffffff",width:"120px",borderRadius:"20px",padding:"5px 10%",float:"right",overflow:"scroll"}}>
+                  <p>Money Earned : </p><h1 style={{color:"green"}}>${income}</h1>
+                </DivScroll>
+                </div>
+              <div style={{width:"50%",color:"#395A86",marginLeft:"3%"}}>
+                <DivScroll style={{backgroundColor:"#ffffff",width:"120px",borderRadius:"20px",padding:"5px 10%",float:"left",overflow:"scroll"}}>
+                    <p>Money Spent : </p><h1 style={{color:"red"}}>${expense}</h1>
+                </DivScroll>         
+                </div> 
+            </div>
             <Reset onClick={reset}>Reset values</Reset>
         </MoneyDiv>
        
         <Form onSubmit={add}>
-          <div style={{display:"flex",flexdirection:"column"}}>
-            <Input style={{width:"70%"}} type="text" placeholder='Description' name='description'onFocus={clear} required></Input>
-            <Input style={{width:"20%"}}type="text" placeholder='Amount' name='amount' onFocus={clear} required></Input>
-            </div>
+            <Input style={{width:"40%"}} type="text" placeholder='Where did I waste my money or did come from' name='description' required></Input>
+            <Input  style={{width:"20%"}}  type="text" placeholder='Amount' name='amount' required></Input>
+
         <Checkbox>
-            <div>
-              <input type="checkbox"   name='incomeType' ></input> 
+            <div style={{margin:"5px 0"}}>
+              <input type="checkbox"   name='incomeType' style={{cursor:"pointer"}}></input> 
               <label>check if it's an income</label>
             </div>
-            <div>
-              <input type="checkbox"   name='expenseType' ></input> 
+            <div style={{margin:"5px 0"}}>
+              <input type="checkbox"   name='expenseType' style={{cursor:"pointer"}}></input> 
               <label>check if it's an expense</label>
             </div>
-            {err && <p style={{color:"red"}}>Check an amount type !!</p>}
           </Checkbox>
             <Button type="submit" >Add</Button>
         </Form>
-        <div style={{margin:"0 15px"}}>
+        <div style={{margin:"0 15%"}}>
           <h1>History :</h1>
           <hr></hr>
           <Ul>
+            <Remove onClick={()=> {
+              var count = 0;
+              for(var i in couple){
+                if(couple.hasOwnProperty(i)) count++;
+              }
+              if(count !== 0){
+                 swal({
+                    title: "Are you sure to remove all the transactions?",
+                    text: "Once deleted, you will not be able to recover the transactions",
+                    icon: "warning",
+                    buttons: true,
+                    dangerMode: true,
+                  })
+                  .then((willDelete) => {
+                    if (willDelete) {
+                      setCouple({});
+                      swal("All transactions deleted!", {
+                        icon: "success",
+                        buttons:false
+                      });
+                    } else {
+                      swal("transactions still here!");
+                    }
+                  });
+              }
+                else if(count === 0){
+                  swal({
+                    text: "No transactions to remove",
+                    buttons: false,
+                    timer:1000
+                  })
+                } 
+            }
+            }>Remove All</Remove>
       {Object.keys(couple).map(key => {
         return <Li key={key}>
-            <p style={{width:"70%"}}>{couple[key]} </p>
-            <p style={{width:"20%"}} >{key} $ </p>
+            <Description style={{width:"70%",overflow:"scroll"}}>{couple[key]} </Description>
+            <Amounts style={{width:"30%",overflow:"scroll"}} >{key}$</Amounts>
             <p><Remove onClick={() => { 
             
               const copyCouple = {};
                 Object.keys(couple).map(key1 => {
+                   var count = 0
+                   for(var i in couple){
+                     if(couple.hasOwnProperty(i)) count++;
+                   }
+                   if(count === 1){
+                    setCouple({})
+                   }
                   while(couple[key1] !== couple[key]){
-                   copyCouple[key1] = couple[key1];
-                   setCouple(copyCouple);
-                   return true;
+                   copyCouple[key1] = couple[key1]
+                   setCouple(copyCouple)
+                   return true
                   }
-                  //
                 })
               ;
               
             }}>remove</Remove></p>
+            
                </Li> 
       })}
           </Ul>
--- budgetapp/src/Styling.js
+++ budgetapp/src/Styling.js
@@ -1,14 +1,16 @@
 import styled from 'styled-components'
 export const Button = styled.button`
 margin: 30px;
-width: 30%;
-padding: 5px 0;
-background-color: #395a86;
+width: 20%;
+padding: 15px 0;
+font-size: 18px;
+background-color: #3D64A4;
 color: #f6f6f6;
-border-radius:10px;
+border-radius:20px;
+border: 1px solid #3D64A4;
 cursor: pointer;
 &:hover{
-    color: #395a86;
+    color:#3D64A4;
     background-color: #f6f6f6;
 }
 `
@@ -16,6 +18,8 @@ export const Form = styled.form`
 display: flex;
 flex-direction: column;
 align-items: center;
+margin-top: 50px;
+
 `
 export const MoneyDiv = styled.div`
 display: flex;
@@ -23,33 +27,55 @@ flex-direction: column;
 text-align: center;
 width: 100%;
 padding: 20px 0;
+color: #ffffff;
+background: #373B44;
+background: -webkit-linear-gradient(to bottom, #4286f4, #373B44);
+background: linear-gradient(to bottom, #4286f4, #373B44);
+
+
 `
 export const Input = styled.input`
-padding: 10px;
 background-color: transparent;
 border: none;
 border-bottom:1px solid #333 ;
 margin: 20px 7px;
+height: 40px;
+font-size: 17px;
+padding: 5px;
+&:focus{
+  border-radius:5px;
+}
 `
 export const Checkbox = styled.div `
 display: flex;
 flex-direction: column;
-text-align: center;
+text-align: start;
+font-size: 17px;
 `
 export const Reset = styled.button`
 margin-top: 30px;
 background-color: transparent;
 border: none;
 cursor: pointer;
-color: #395a86;
+color: #ffffff;
+text-decoration: underline;
+font-size: 15px;
 `
 export const Ul = styled.ul`
 list-style: none;
-background-color: aliceblue;
+margin: 0;
+padding:0;
 `
 export const Li = styled.li`
 display: flex;
 flex-direction: row;
+font-size: 18px;
+background-color: #ffffff;
+margin: 0 10px;
+margin-top: 20px;
+padding: 0 10px;
+height: 70px;
+border-radius:10px;
 `
 export const Remove = styled.button`
 margin-right: 10px;
@@ -59,4 +85,18 @@ border: none;
 border-bottom: solid 1px #395a86;
 cursor: pointer;
 color: #395a86;
+font-size: 15px;
+
+`
+export const Description = styled.p`
+margin-right: 20px;
+font-weight: 500;
+&::-webkit-scrollbar {display:none;}
+`
+export const Amounts = styled.p`
+&::-webkit-scrollbar {display:none;}
+`
+export const DivScroll = styled.div`
+&::-webkit-scrollbar {display:none;}
+
 `
